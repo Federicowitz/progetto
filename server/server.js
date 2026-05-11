a4cb45f859110678a9577622b5844421f620108b
Federico <federicolelli93@gmail.com>
2026-05-11 20:36:35 +0200

    added third note

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ app.use(express.json());
 
 let notes = [
   { id: 1, text: "Prima notassssss" },
-  { id: 2, text: "Seconda nota e bastaaaaaasecondo" }
+  { id: 2, text: "Seconda nota e bastaaaaaasecondo" },
+  { id: 3, text: "terza" }
 ];
 
 
